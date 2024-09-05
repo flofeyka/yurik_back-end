25d07f0280a7ea3c824a905d1f0e9eb0fa737e13
flofeyka <114827934+flofeyka@users.noreply.github.com>
2024-09-05 15:05:56 +0500

    Update login-dto.ts

--- src/auth/dtos/login-dto.ts
+++ src/auth/dtos/login-dto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class LoginDto {
-    @ApiProperty({title: "Номер телефона", example: "79993001234"})
-    phoneNumber: number;
+    @ApiProperty({title: "Номер телефона", example: "+79993001234"})
+    phoneNumber: string;
 
     @ApiProperty({title: "Пароль(Временно)", example: "qwerty1234"})
     password: string;

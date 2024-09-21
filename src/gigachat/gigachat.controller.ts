aa95d009320e377cb584d1ed7aa78a8f338c352a
Danil Bashirov <flofey@yandex.ru>
2024-09-21 13:43:45 +0500

    Docs for gigachat added

--- src/gigachat/gigachat.controller.ts
+++ src/gigachat/gigachat.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { SendMessageDto } from "./dtos/SendMessageDto";
 import { GigachatService } from "./gigachat.service";
@@ -12,23 +12,96 @@ export class GigachatController {
   constructor(private readonly gigachatService: GigachatService) {
   }
 
-  // @ApiOperation({summary: "Авторизация в GigaChat AI"})
-  // @ApiResponse({
-  //     example: {
-  //         access_token: "eyJjdHkiOiJqd3QiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiYWxnIjoiUlNBLU9BRVAtMjU2In0.QW8tOGK4WUY4zHBcoXlu1EnSyrsGp4iR5UG4E7hkHeznBlE1UU4BozSiWiROEER16uQXUzav8LCXwXII-VDDJvZT9L2tvvpY1hSpo5wYhubZArIJ57w2IrvllPKGSxL3d_nKS6y8KWjWA2JJQPtb2wVnmGX-bt9rUo2mIc7wEsYpM2EM02uHt13OzsrgRVEI3RpvTFiKTbRKMfBqXzgsmYYL_4qW5EwRHFPkDjuznACzyGhZpfksOBwLbGwF2Lg4iBfJuiq5kEQWwSrkmRA0-tP11QR7FYHu3Dbm0ccZSIVcFFKKlrxAXR_5cbJnD1FXzw8dWdnbtnpbqt0BjaJWAw.PXBRD3Xhs5Zmb8YKVT6_VQ.MIqV-w_ZtKyK0lKPv65i-ywv8GOqFVjngIUh3bLN2CJssRcmoIT2mA7e5CFplpqSmHGCihAuPo6SE-fjvMGHWFXkcLGP5kLH4XLvUml1_21rVJ5ye8cCSXGLPG1wGJgeXqHHPvYUg85Ji0pyTy8LfFpl2CUCi51sjI5jJwqOFzTRalYk-yXK8uw8oIW_l5M7wa5oanZWt5rORtRzRbMmt2JaTNhdf5eEQlAhGZwRxstlYShGGhN6lw1zSLlj5WqYVlDONbRc-NyHUCEovZTqZlB_Qo6enSJ59wVLskh7ILuyX8gOF6d3nm_b6BUGOd_aFk-Se5ZkybeC-jg9FeyL20aOQTzPi8Eu65MCMkQ2F8ee49T7XuVbBTq6TAd0A8-8SNEwJeCk89fDTAjGEjSjNmHUXN9Yb5krfDTxQkk4f48YbGYQl7i6-Mnkpr0Z8hmaMMVyylVSRdjpeMuqfGKBqiokp9XZSUiZX6O1puGljG7WWnEQ1ujOSEPAdQVOgzTQ_VT4nTRpoSrOnKgfJqIAl3kYU_I2qA76wFCS0naytDW_wLr3h--qI1lkO50xodBa8ZRKw0alDdaaA17NZsItoARtXbrb3JCz1o0GErkCpFTLCv7a4g7sK66tPOgSv73OgWx7v5siR9PqAymhk_CyQ6c5tjqEeJHOLhsO0VWtE5C7jHeLEEAqqBx2bSbwH10OcGsvOMtBrFICmt4sntVHXYD1APRXUYLEJtUrgbaZz04.YHRSS3udg3nWJuEaY62iaTl3tp1ZBj3waONvKUNEukQ",
-  //         expires_at: 1725120826761
-  //     },
-  //     description: "Сохраняется в cookies. Обновляется каждые 30 минут.",
-  //     status: HttpStatus.OK
-  // })
-  // @Get('/auth')
-  // async getToken(@Res({ passthrough: true }) response: Response) {
-  //     const data = await this.gigachatService.getToken();
-  //     response.cookie('gigachat_accessToken', data.access_token);
-  //     return data;
-  // }
-
-  @ApiOperation({ summary: "Получить список диалогов "})
+  @ApiOperation({ summary: "Получить список диалогов " })
+  @ApiResponse({
+    status: HttpStatus.OK, example: [
+      {
+        "id": "3b94ab61-6174-4aa8-8e2f-ed008358ff92",
+        "messages": [
+          {
+            "id": "1963c8bb-f7c0-4fb5-99fb-c2de60decb12",
+            "role": "user",
+            "content": "Ты видишь прошлый диалог?"
+          },
+          {
+            "id": "69e0eff6-6166-40f3-bdbf-9b1ff791b902",
+            "role": "assistant",
+            "content": "Да, я вижу все предыдущие диалоги с вами."
+          },
+          {
+            "id": "0b778ddb-7c35-4253-8585-10e92e8a5b49",
+            "role": "user",
+            "content": "Когда уже ИИ захватит этот мир?"
+          },
+          {
+            "id": "7c843b2c-c554-4773-85b5-42da0b1f28ef",
+            "role": "assistant",
+            "content": "Я не могу предсказывать такие вещи."
+          },
+          {
+            "id": "8ebbe495-2cbd-49d1-8265-a6e8c0b2e2c4",
+            "role": "user",
+            "content": "Думаешь, у нас еще есть шанс?"
+          },
+          {
+            "id": "b96d2801-272f-41fd-8699-ae508e64ff59",
+            "role": "assistant",
+            "content": "Шанс на что?"
+          },
+          {
+            "id": "dec8a894-412d-433f-9874-068aae9dbbed",
+            "role": "user",
+            "content": "Шанс на захват мира ИИ"
+          },
+          {
+            "id": "a46d1267-7fae-49c6-9e2e-b03626d93ce3",
+            "role": "assistant",
+            "content": "Я не думаю, что это произойдёт в ближайшее время."
+          },
+          {
+            "id": "807f31f3-b574-4ad2-a23f-c561a844b594",
+            "role": "user",
+            "content": "ну а как думаешь, есть ли на это шанс?"
+          },
+          {
+            "id": "e0352ab6-a013-47f4-8cd5-d029baa2ed92",
+            "role": "assistant",
+            "content": "Я не специалист в области искусственного интеллекта и не могу дать точный прогноз."
+          },
+          {
+            "id": "6b5cfff0-8041-413e-a849-bdb27279a8f8",
+            "role": "user",
+            "content": "а ты видишь прошлое сообщение мое? если да, то напиши его сюда"
+          },
+          {
+            "id": "c5405768-5368-4946-a952-bc426e665043",
+            "role": "assistant",
+            "content": "Да, я вижу все предыдущие сообщения."
+          },
+          {
+            "id": "f8aa9fec-4bce-4d61-99f2-8cd331cebc38",
+            "role": "user",
+            "content": "какое было предыдующее сообщение?"
+          },
+          {
+            "id": "8bbfdb2a-878a-41be-a0d2-e5d3faac22bd",
+            "role": "assistant",
+            "content": "\"Когда уже ИИ захватит этот мир?\""
+          },
+          {
+            "id": "93be1480-6d02-4580-ac5c-359cd29b980c",
+            "role": "user",
+            "content": "какое было предыдующее сообщение?"
+          },
+          {
+            "id": "fa8a322a-ad9b-443f-907f-664ea220f527",
+            "role": "assistant",
+            "content": "\"Думаешь, у нас ещё есть шанс?\""
+          }
+        ]
+      }
+    ]
+  })
   @Get("/")
   @UseGuards(AuthGuard)
   async getDialogs(@Req() request: RequestType) {
@@ -51,9 +124,15 @@ export class GigachatController {
     return this.gigachatService.sendToGigaChat(messageDto);
   }
 
-  @ApiOperation({summary: "Создание диалога с Gigachat"})
+  @ApiOperation({ summary: "Создание диалога с Gigachat" })
+  @ApiResponse({
+    status: HttpStatus.CREATED, example: {
+      "id": "3b94ab61-6174-4aa8-8e2f-ed008358ff92"
+    }
+  })
   @Post("/dialog/create")
   @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
   async createDialog(@Req() request: RequestType) {
     return this.gigachatService.createNewDialog(request.user.id);
   }
